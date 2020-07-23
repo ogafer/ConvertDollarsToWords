cd504ca61eace360ffda25abe8eb0fd005d1cef7
ogafer <68665500+ogafer@users.noreply.github.com>
2020-07-23 12:43:15 +0100

    Add files via upload
    First commit - WPF client

---
+++ WPF_Code_Challenge/Dolars_In_words/Dolars_In_words/ConvertToWords.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dollars_In_words
+{
+    class ConvertToWords
+    {
+        #region Properties
+        public string WordsOfDollars { get; set; }
+        public string WordsOfCents { get; set; }
+        #endregion
+
+        #region Constructor
+
+        public ConvertToWords()
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+        public string DollarsToWords(long dollars, int cents = 0)
+        {
+            if (dollars > 999999999 || cents > 99)
+                return "Maximum of 999999999 dollars or 99 cents surpassed try with a smaller number";
+
+            if (dollars == 0)
+                return "zero";
+
+            if (dollars < 0)
+                return "Not possible to have more than 0 dollars!";
+
+            WordsOfDollars = "";
+            WordsOfCents = "";
+
+            if ((dollars / 1000000) > 0)
+            {
+                WordsOfDollars += DollarsToWords(dollars / 1000000) + " million ";
+                dollars %= 1000000;
+            }
+
+            if ((dollars / 1000) > 0)
+            {
+                WordsOfDollars += DollarsToWords(dollars / 1000) + " thousand ";
+                dollars %= 1000;
+            }
+
+            if ((dollars / 100) > 0)
+            {
+                WordsOfDollars += DollarsToWords(dollars / 100) + " hundred ";
+                dollars %= 100;
+            }
+
+            if (dollars > 0)
+            {
+                if (WordsOfDollars != "")
+                    WordsOfDollars += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (dollars < 20)
+                {
+                    WordsOfDollars += unitsMap[dollars];
+                }
+                else
+                {
+                    WordsOfDollars += tensMap[dollars / 10];
+                    if ((dollars % 10) > 0)
+                    {
+                        WordsOfDollars += "-" + unitsMap[dollars % 10];
+                    }
+                }
+            }
+            #region Cents
+            if (cents > 0)
+            {
+                if (WordsOfCents != "")
+                    WordsOfCents += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (cents < 20)
+                {
+                    WordsOfCents += unitsMap[cents];
+                }
+                else
+                {
+                    WordsOfCents += tensMap[cents / 10];
+                    if ((cents % 10) > 0)
+                    {
+                        WordsOfCents += "-" + unitsMap[cents % 10];
+                    }
+                }
+            }
+            #endregion
+            bool noCents = string.IsNullOrEmpty(WordsOfCents);
+            return WordsOfDollars + " dollars " + (noCents? "" : "and " + WordsOfCents + " cents");
+        }
+        #endregion
+    }
+}
---
+++ WPF_Code_Challenge/Dolars_In_words/Dolars_In_words/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Dollars_In_words;
+
+namespace Dollars_In_words
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables
+        ConvertToWords convertToWords = new ConvertToWords();
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnQuit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_convertToWording(object sender, RoutedEventArgs e)
+        {
+            Regex letters = new Regex(@"([\p{L}\s]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (letters.IsMatch(txtDollars.Text))
+                MessageBox.Show("You have a letter... Try again!");
+            else
+            {
+                if (string.IsNullOrEmpty(txtDollars.Text))
+                    MessageBox.Show("Write a number!");
+                else
+                {
+                    if (txtDollars.Text.Contains(","))
+                    {
+                        string[] dollarsAndCents = txtDollars.Text.Split(',');
+
+                        string dollars = dollarsAndCents[0];
+                        string cents = dollarsAndCents[1].PadRight(2, '0');
+                        txtShowWording.Text = convertToWords.DollarsToWords(Int64.Parse(dollars), Int32.Parse(cents));
+                    }
+                    else
+                    {
+                        try
+                        {
+                            txtShowWording.Text = convertToWords.DollarsToWords(Int64.Parse(txtDollars.Text));
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Unknow character! Try again!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
